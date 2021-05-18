c8ece21e8fa0a5708a02d0a4739587c1bf1616a9
mingu-lee <kcg3798@naver.com>
2021-05-18 15:45:43 +0900

    day 3 commit

--- day3/3.py
+++ day3/3.py
@@ -1,20 +1,37 @@
-def gun(t,r,c,l):
-    while r+1 < len(l):
-        r +=1
-        c +=3
 
-        way = l[r][c % len(l[r])]
+from functools import reduce
+
+def toboggan(treeCount,row,col,lines):
+    r =0
+    c =0
+    while r+1 < len(lines):
+        r +=row
+        c +=col
+        way = lines[r][c % len(lines[r])]
         if way == "#":
-            t +=1
-    return t
+            treeCount +=1
+    return treeCount
+
+def multiply(arr):
+    return reduce(lambda x, y: x * y, arr)
 
 if __name__ == "__main__":
     f = open("day3/puzzle.txt", 'r')
     lines = f.readlines()
     lines = [line.strip() for line in lines]
 
-    treeCount =0
-    row =0
-    col =0
 
-    print(gun(treeCount, row, col, lines))
+    treeCount =0
+    row =1
+    col =3
+    print("part1= ",toboggan(treeCount, row, col, lines))
+
+    answer = []
+    row =[1,1,1,1,2]
+    col =[1,3,5,7,1]
+
+    for (i,j) in zip(row,col):
+        answer.append(toboggan(treeCount, i, j, lines))
+    print("part2= ",multiply(answer))
+
+#문제가 바뀔때 대응훈련 함수를 나누자
